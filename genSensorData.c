b7b81d7a98996fb8491fbfa7f11e3f9568d68a54
bob92-dev <casselboris92@gmail.com>
2020-04-21 17:02:50 +0200

    story 2 : creation et execution de genSensorData

---
+++ genSensorData.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    printf("Hello welcome to the gensensordata software by antoine, boris and Romain the small dicks\n");
+
+    return 0;
+}
