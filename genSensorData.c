5e5e9f86972433c8c60642b3dca0f07cd4fbbc9f
Inarritu <romaindupuis@gmail.com>
2020-04-22 10:35:04 +0200

    story 4

--- genSensorData.c
+++ genSensorData.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
+#include<string.h>
 #define BUFFERSIZE 1000
 
 int main(int argc, char *argv[]){
 	printf("welcome to mon fion\n");
 
 	char buffer[BUFFERSIZE];
+	while (fgets(buffer,BUFFERSIZE,stdin)){
+	//printf("buffer=%s\n", buffer);
+	if( strcmp(buffer,"OK\n")==0){
 
-	while (	fgets(buffer,BUFFERSIZE,stdin))
-	{
-		printf("%s", buffer);
+		printf("toutes les portes sont bien fermées");
+	}
+	else if ( strcmp(buffer,"ERROR\n")==0) {
+
+		printf("Attention la porte de l'entrée est ouverte!");
+	}
 	}
 
 	return 0;
