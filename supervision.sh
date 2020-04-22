77697461b7975ff0fb5200a4058601fab864c5d9
bob92-dev <casselboris92@gmail.com>
2020-04-22 19:09:01 +0200

    ajout story 8 et 9 pas finies

--- supervision.sh
+++ supervision.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -x
+set -x
 
 timer=$1
 Dir=$2
@@ -7,9 +7,26 @@ fileName1=$3
 fileName2=$4
 fileSize=$5
 
+generationPID=0
 
+# 1) declarer un generationPID=0
+while true;do
+#verifier que generation.sh n'existe pas=> si generatiopnPID=0, il faut le lancer 
+        if [[ $generationPID -eq 0 ]];then 
+                ./generation.sh $1 $2 $3 $4 &
+                generationPID=$!          
+#regarder si le sfichiers de logs pas trop gros => methode : du ou autre ou stat
+                if [[du -h /home/$USER/$Dir/$3] -gt 1 ];then 
+                echo "ceci est presque la fin de la story 9"
+                # si trop gros => kill du PID => copier fichiers etc => PID=0
+                kill $!
+                fi
+        fi                
 
-if [ -e /home/$USER/$Dir ];then
+sleep 5
+done
+
+if [ -e /home/$USER/$Dir/ ];then
 
         echo "le fichier existe"
 
