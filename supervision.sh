cf9ea0bfd7a559b09d91eb29cd080a3c6dc78f2d
Inarritu <romaindupuis@gmail.com>
2020-04-23 16:27:54 +0200

    story 8/9 ok?

--- supervision.sh
+++ supervision.sh
@@ -9,28 +9,54 @@ fileSize=$5
 
 generationPID=0
 
+
+
 # 1) declarer un generationPID=0
 while true;do
+# verifier qui est l'utilisateur courant et recuprerla taille des fichiers de logs
+testSize=$(du /../home/$USER/$Dir/$fileName1.log | cut -f1);
+testSizeError=$(du /../home/$USER/$Dir/$fileName2.log | cut -f1);
+
+#message si le dossier n'existe pas ou si le script n'est pas lancé
+#process = $(pgrep generation.sh)
+#if [[ $process -eq 0 ]]; then 
+#        echo " coucou tu veux voir mon process ? je ne suis pas lancé"
+#fi
+
+#testSizeCut=$(echo $testSize | cut -d "k")
 #verifier que generation.sh n'existe pas=> si generatiopnPID=0, il faut le lancer 
-        if [[ $generationPID -eq 0 ]];then 
-                ./generation.sh $1 $2 $3 $4 &
-                generationPID=$!          
+        if [[ $generationPID -eq 0 ]]; then
+                ./generation.sh $timer $Dir $fileName1 $fileName2 &
+                generationPID=$!
+
 #regarder si le sfichiers de logs pas trop gros => methode : du ou autre ou stat
-                if [[du -h /home/$USER/$Dir/$3] -gt 1 ];then 
-                echo "ceci est presque la fin de la story 9"
+
+        fi
+        if [[ $testSize -lt $fileSize && $testSizeError -lt $fileSize ]]; then 
+                echo "dans le if la taille est $testSize"
+                echo "la taille est plus petite que celle donnée en argument"
+
+        else
+                echo "la taille est trop grande"
                 # si trop gros => kill du PID => copier fichiers etc => PID=0
-                kill $!
-                fi
-        fi                
+                #kill $generationPID
+                pgrep generation.sh | xargs kill
+                date=$(date '+%Y-%m-%d')
+                
+                tar -czvf $date.tar.gz /../home/$USER/$Dir 
+                generationPID=0
+        fi
+
 
 sleep 5
 done
 
-if [ -e /home/$USER/$Dir/ ];then
+if [ -e /home/$USER/$Dir/ ]; then
 
         echo "le fichier existe"
 
+
  else
 
-          echo "le fichier n'existe pas garçon!"       
-fi        
+          echo "le fichier n'existe pas garçon!"
+fi
