edd2efa46f4e95ec87699f88b3bc81a617c6d928
Inarritu <romaindupuis@gmail.com>
2020-04-24 12:53:29 +0200

    début comments anglais

--- supervision.sh
+++ supervision.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -x
 
+#list of args timer is the time between the display, 
+#Dir is for the directory the we create
+#fileName1 is one of the log file the we create
+#fileName2 is one of the log file the we create
+#fileSize is the maximum size the we set for the log's file
+
 timer=$1
 Dir=$2
 fileName1=$3
@@ -12,10 +18,12 @@ version=0
 
 
 
-# 1) declarer un generationPID=0
+# Start of the infinite loop
 while true;do
 # verifier qui est l'utilisateur courant et recuprerla taille des fichiers de logs
+# get back the log file and calculate its size
 testSize=$(du /home/$USER/$Dir/$fileName1.log | cut -f1);
+# get back the log file and calculate its size
 testSizeError=$(du /home/$USER/$Dir/$fileName2.log | cut -f1);
 
 #message si le dossier n'existe pas ou si le script n'est pas lancé
@@ -25,7 +33,7 @@ testSizeError=$(du /home/$USER/$Dir/$fileName2.log | cut -f1);
 #fi
 
 #testSizeCut=$(echo $testSize | cut -d "k")
-#verifier que generation.sh n'existe pas=> si generatiopnPID=0, il faut le lancer 
+#verify if "generation.sh" is launched and launch it , if it's not done  
         if [[ $generationPID -eq 0 ]]; then
                 ./generation.sh $timer $Dir $fileName1 $fileName2 &
                 generationPID=$!
@@ -38,30 +46,32 @@ testSizeError=$(du /home/$USER/$Dir/$fileName2.log | cut -f1);
         echo $?
         #test si ca renvoie 1 ou 0 => ensuite l'inclure dans la condition
 
+
+#condition where we verify if logfile's size are greater than the size set in argument $fileSize
         if [[ $testSize -gt $fileSize || $testSizeError -gt $fileSize ]]; then 
 
                 echo "la taille du fichier est trop grande. Enclenchement procedure archive"
                 # si trop gros => kill du PID => copier fichiers etc => PID=0
-                #kill $generationPID
+                # if the size is greater : get back the genereation.sh PIDS and kill them
                 pgrep generation.sh | xargs kill
                 date=$(date '+%Y-%m-%d')
 
                 cut -d: -f2,3,4,5 /home/$USER/$Dir/$fileName1.log | sort -n -o /home/$USER/$Dir/$fileName1.log
                 exit 0
-                # comptage du nombres enregistrements dans fichiers 1 et 2
-                # et enregistrement dans un fichier numberInLog
+                # count numberts of records in logfiles 
+                # save this count in a new file with name numberInLog
                 touch /home/$USER/$Dir/numberInLog.txt
                 wc -l /home/$USER/$Dir/$fileName1.log >> /home/$USER/$Dir/numberInLog.txt  
                 wc -l /home/$USER/$Dir/$fileName2.log >> /home/$USER/$Dir/numberInLog.txt
 
-                # archivage des logs dans le même repertoire et du fichier numberInLog       
+                # compress the logfiles and numberInLog in the same directory     
                 cd /home/$USER/$Dir
                 tar -czvf $date.tar.gz *.log *.txt
                 cd -
-                # suppression des fichiers d'origine avant reinit du process 
+                # delete the files before reinitialize 
                 rm /home/$USER/$Dir/*.log
                 rm /home/$USER/$Dir/*.txt
-                #reinit du PID
+                #reinitialize the PID to 0
                 generationPID=0
         fi
 
